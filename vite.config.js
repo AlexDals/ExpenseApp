8df47bd13dfbf711b900e362e60557debddcd707
AlexDals <100142352+AlexDals@users.noreply.github.com>
2025-06-09 13:10:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://uxhdokrbezwyftllsohx.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4aGRva3JiZXp3eWZ0bGxzb2h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0Nzc1MjcsImV4cCI6MjA2NTA1MzUyN30.IiJZS7_PX_swPXXDWtQrIWx0kIvKMUVm4T8MK5LuQOQ')
   }
 });
